8edde9b0665626059ce30546bf9b9dbbd7f532cd
Mohamed Rizad <rizadvillan@gmail.com>
2023-04-03 01:04:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ msg_buttons=InlineKeyboardMarkup(
 # Fillout The variables in Config.py further queries @riz4d 0n telegram
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
-APP_ID = int(os.environ.get("API_ID"))
+API_ID = int(os.environ.get("API_ID"))
 API_HASH = os.environ.get("API_HASH")
 
 app=Client('Temp-Mail Bot',
