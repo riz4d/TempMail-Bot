adfe12950153308de229cae3b954338a45d6f6f6
Mohamed Rizad <rizadvillan@gmail.com>
2023-04-03 01:15:51 +0530

    minor patches 23

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ app=Client('Temp-Mail Bot',
 email=''
 @app.on_message(filters.command('start'))
 async def start_msg(client,message):
-    await message.reply("**Hey❕**\n @mysterymailbot is a free service that allows to generates and receive emails at a temporary address that self-destructed after a certain time elapses.\n\n**__ How It Safe's You??**__\n- Using the temporary mail allows you to completely protect your real mailbox against the loss of personal information. Your temporary e-mail address is completely anonymous. Your details: information about your person and users with whom you communicate, IP-address, e-mail address are protected and completely confidential.\n\nFurther Queris @riz4d🌚")
+    await message.reply("**Hey "+message.from_user.first_name+" !!**\n @mysterymailbot is a free service that allows to generates and receive emails at a temporary address that self-destructed after a certain time elapses.\n\n**__ How It Safe's You??**__\n- Using the temporary mail allows you to completely protect your real mailbox against the loss of personal information. Your temporary e-mail address is completely anonymous. Your details: information about your person and users with whom you communicate, IP-address, e-mail address are protected and completely confidential.\n\nFurther Queris @riz4d🌚")
     await message.reply("**Generate a Email Now❕**",
                         reply_markup=buttons)
 @app.on_callback_query()
@@ -95,6 +95,8 @@ async def mailbox(client,message):
             filedl=wget.download(attc)
             await message.edit_message_text(mailbox_vieww,reply_markup=buttons)
             os.remove(dlattach)
+    elif response=='close':
+        await message.edit_message_text('Session Closed✅')
 app.run()
 
 # Stay tuned for more : Instagram[@riz.4d]
